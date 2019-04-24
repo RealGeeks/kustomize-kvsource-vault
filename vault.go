51caab951fef35a6cc5235843c13232932f4cdde
Lars Levie <larslevie@gmail.com>
2019-04-24 11:36:28 -0500

    Prefix secret keys with the name of the Vault secret

--- vault.go
+++ vault.go
@@ -34,27 +34,27 @@ func (p plugin) Get(root string, args []string) (map[string]string, error) {
 	for _, k := range args {
 		var t = strings.Split(k, "//")
 		var path = t[0]
+		var splitPath = strings.Split(path, "/")
+		var secretPrefix = splitPath[len(splitPath)-1]
 		var key = t[1]
 
 		secret, err := client.Logical().Read(path)
-
 		if err != nil {
 			fmt.Println(err)
 			return nil, err
 		}
-
 		if secret == nil {
 			return nil, fmt.Errorf("the path %q was not found", path)
 		}
 
 		vaultSecretData, ok := secret.Data["data"].(map[string]interface{})
-
 		if !ok {
 			return nil, fmt.Errorf("malformed ecret data: %q", secret.Data["data"])
 		}
 
-		mapOfSecrets[key], ok = vaultSecretData[key].(string)
+		secretKey := fmt.Sprintf("%s_%s", secretPrefix, key)
 
+		mapOfSecrets[secretKey], ok = vaultSecretData[key].(string)
 		if !ok {
 			return nil, fmt.Errorf("%q was not found at %q", key, path)
 		}
